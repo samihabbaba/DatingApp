fe95fe3744c10bda3c30f15b3d871e1d40a61acd
sammy2h <sami_habbaba30@hotmail.com>
2020-09-12 23:21:53 +0300

    End of section 10

--- DatingApp2-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp2-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { AuthService } from 'src/app/_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
 import { User } from '../../_models/user';
 
 @Component({
@@ -8,14 +12,30 @@ import { User } from '../../_models/user';
   styleUrls: ['./member-edit.component.scss']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm', {static: true}) editForm: NgForm;
   user: User;
+  @HostListener('window:beforeload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService,
+    private userService: UserService, private authService: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data['user'];
-    })
+    });
   }
 
+  updateUser() {
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+      this.alertify.success('Profile updated successfully');
+      this.editForm.reset(this.user);
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
 }
--- DatingApp2.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp2.API/Helpers/AutoMapperProfiles.cs
@@ -21,6 +21,7 @@ namespace DatingApp2.API.Helpers
                 .ForMember(dest => dest.Age, opt => 
                     opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
